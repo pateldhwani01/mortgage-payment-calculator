b402b4c771e907c68d59e179577d33075c2cdd4b
Dave Mooney <hello@dave-mooney.co.uk>
2020-09-19 13:52:23 +0100

    Small edits

--- src/components/CalculatorControls.js
+++ src/components/CalculatorControls.js
@@ -4,7 +4,6 @@ export class CalculatorControls extends React.Component {
 	render() {
 		return (
 			<div className="grid__container">			
-
 				<div className="grid__item">
 					<span className="grid__item--header">£{this.props.numberWithCommas(this.props.purchasingHousePrice)}</span>
 					<input 
@@ -12,7 +11,7 @@ export class CalculatorControls extends React.Component {
 						className="grid__item--range-slider"
 						id="purchasingHousePrice"  
 						min="50000" 
-						max="500000" 
+						max="750000" 
 						step="1000" 
 						defaultValue="285000" 
 						onChange={() => this.props.updateValues(
@@ -22,16 +21,30 @@ export class CalculatorControls extends React.Component {
 							document.getElementById('interestRate').value)} />
 					<label className="grid__item--label" htmlFor="purchasingHousePrice">Purchasing House Price</label>
 				</div>
-
-				
-
+				<div className="grid__item">
+					<span className="grid__item--header">£{this.props.numberWithCommas(this.props.depositAmount)}</span>
+					<input 
+						type="range"
+						className="grid__item--range-slider"
+						id="depositAmount" 
+						min="1000" 
+						max="150000" 
+						step="1000" 
+						defaultValue="72000" 
+						onChange={() => this.props.updateValues(
+							document.getElementById('depositAmount').value, 
+							document.getElementById('purchasingHousePrice').value, 
+							document.getElementById('mortgageTerm').value, 
+							document.getElementById('interestRate').value)} />
+					<label className="grid__item--label" htmlFor="points">Deposit</label>
+				</div>
 				<div className="grid__item">
 					<span className="grid__item--header">{this.props.numberWithCommas(this.props.mortgageTerm)} Years</span>
 					<input 
 						type="range"
 						className="grid__item--range-slider"
 						id="mortgageTerm"  
-						min="15" 
+						min="5" 
 						max="35" 
 						step="1" 
 						defaultValue="25" 
@@ -42,7 +55,6 @@ export class CalculatorControls extends React.Component {
 							document.getElementById('interestRate').value)} />
 					<label className="grid__item--label" htmlFor="mortgageTerm">Mortgage Term</label>
 				</div>
-
 				<div className="grid__item">
 					<span className="grid__item--header">{this.props.numberWithCommas(this.props.interestRate)}%</span>
 					<input 
@@ -60,25 +72,6 @@ export class CalculatorControls extends React.Component {
 							document.getElementById('interestRate').value)} />
 					<label className="grid__item--label" htmlFor="interestRate">Interest Rate</label>
 				</div>
-
-				<div className="grid__item">
-					<span className="grid__item--header">£{this.props.numberWithCommas(this.props.depositAmount)}</span>
-					<input 
-						type="range"
-						className="grid__item--range-slider"
-						id="depositAmount" 
-						min="1000" 
-						max="100000" 
-						step="1000" 
-						defaultValue="72000" 
-						onChange={() => this.props.updateValues(
-							document.getElementById('depositAmount').value, 
-							document.getElementById('purchasingHousePrice').value, 
-							document.getElementById('mortgageTerm').value, 
-							document.getElementById('interestRate').value)} />
-					<label className="grid__item--label" htmlFor="points">Deposit</label>
-				</div>		
-
 			</div>
 		);
 	}
