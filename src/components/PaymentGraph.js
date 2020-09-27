6c964c80f123e2c228f1968a6516c2dc8536feb4
Dave Mooney <hello@dave-mooney.co.uk>
2020-09-27 20:55:36 +0100

    Added a bar chart for Capital repaid and Interest paid

--- src/components/PaymentGraph.js
+++ src/components/PaymentGraph.js
@@ -1,72 +1,72 @@
 import React from 'react';
 import {Line} from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
 
 const graphData = {
   labels: [],
-  datasets: [
-    {
-      label: 'Mortgage Balance',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(0,140,255,0.4)',
-      borderColor: 'rgba(0,140,255,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(0,140,255,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(0,140,255,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [],
+  datasets: [{
+    	label: 'Mortgage Balance',
+    	fill: false,
+    	lineTension: 0.1,
+    	backgroundColor: 'rgba(0,140,255,0.4)',
+    	borderColor: 'rgba(0,140,255,1)',
+    	borderCapStyle: 'butt',
+    	borderDash: [],
+    	borderDashOffset: 0.0,
+    	borderJoinStyle: 'miter',
+    	pointBorderColor: 'rgba(0,140,255,1)',
+    	pointBackgroundColor: '#fff',
+    	pointBorderWidth: 1,
+    	pointHoverRadius: 5,
+    	pointHoverBackgroundColor: 'rgba(0,140,255,1)',
+    	pointHoverBorderColor: 'rgba(220,220,220,1)',
+    	pointHoverBorderWidth: 2,
+    	pointRadius: 1,
+    	pointHitRadius: 10,
+    	data: []
     },
     {
-      label: 'Interest Paid',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(249,31,116,0.4)',
-      borderColor: 'rgba(249,31,116,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(249,31,116,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(249,31,116,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [],
+    	label: 'Interest Paid',
+    	fill: false,
+    	lineTension: 0.1,
+    	backgroundColor: 'rgba(249,31,116,0.4)',
+    	borderColor: 'rgba(249,31,116,1)',
+    	borderCapStyle: 'butt',
+    	borderDash: [],
+    	borderDashOffset: 0.0,
+    	borderJoinStyle: 'miter',
+    	pointBorderColor: 'rgba(249,31,116,1)',
+    	pointBackgroundColor: '#fff',
+    	pointBorderWidth: 1,
+    	pointHoverRadius: 5,
+    	pointHoverBackgroundColor: 'rgba(249,31,116,1)',
+    	pointHoverBorderColor: 'rgba(220,220,220,1)',
+    	pointHoverBorderWidth: 2,
+    	pointRadius: 1,
+    	pointHitRadius: 10,
+    	data: []
     },
     {
-      label: 'Capital Repaid',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [],
-    },
+    	label: 'Capital Repaid',
+    	fill: false,
+    	lineTension: 0.1,
+    	backgroundColor: 'rgba(75,192,192,0.4)',
+    	borderColor: 'rgba(75,192,192,1)',
+    	borderCapStyle: 'butt',
+    	borderDash: [],
+    	borderDashOffset: 0.0,
+    	borderJoinStyle: 'miter',
+    	pointBorderColor: 'rgba(75,192,192,1)',
+    	pointBackgroundColor: '#fff',
+    	pointBorderWidth: 1,
+    	pointHoverRadius: 5,
+    	pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+    	pointHoverBorderColor: 'rgba(220,220,220,1)',
+    	pointHoverBorderWidth: 2,
+    	pointRadius: 1,
+    	pointHitRadius: 10,
+    	data: []
+    }
   ]
 };
 const graphOption = {
@@ -81,24 +81,54 @@ const graphOption = {
     }
 }
 
+const barGraphData = {
+    	datasets: [{
+    		label: 'Capital Repaid',
+    		backgroundColor: 'rgba(75,192,192,0.4)',
+        	data: []
+    	},
+    	{
+    		label: 'Interest Repaid',
+    		backgroundColor: 'rgba(249,31,116,0.4)',
+        	data: []
+    	}],
+    	labels: []
+}
+
 export class PaymentGraph extends React.Component {
 	render() {
 		graphData.labels = [];
 		graphData.datasets[0].data = [];
 		graphData.datasets[1].data = [];
 		graphData.datasets[2].data = [];
+		barGraphData.labels = [];
+		barGraphData.datasets[0].data = [];
+		barGraphData.datasets[1].data = [];
   		for(let i = 0; i <= this.props.mortgageTerm; i++) {
 			graphData.labels.push(i);
 			graphData.datasets[0].data.push(this.props.yearlyPayments[i].outstandingBalance);
 			graphData.datasets[1].data.push(this.props.yearlyPayments[i].interestPaidToDate);
 			graphData.datasets[2].data.push(this.props.yearlyPayments[i].capitalRepaidToDate);
+			if (i > 0) { //Stops year 0 with no money paid from appearing on bar chart
+				barGraphData.labels.push(i);
+				barGraphData.datasets[0].data.push(this.props.yearlyPayments[i].capitalRepaid);
+				barGraphData.datasets[1].data.push(this.props.yearlyPayments[i].interestPaid);
+			}
 		}
     	return(
-    		<div className="graph-container">
-    			<Line 
-    				data={graphData}
-    				options={graphOption}
-    			/>
+    		<div>
+    			<div className="graph-container">
+    				<Line 
+    					data={graphData}
+    					options={graphOption}
+    				/>
+    			</div>
+    			<div className="graph-container">
+    				<Bar 
+    					data={barGraphData}
+    					options={graphOption}
+    				/>
+    			</div>
     		</div>
     	)
   	}
